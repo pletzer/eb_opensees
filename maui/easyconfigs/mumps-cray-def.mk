c82de3bb22c90814fdd83a37da0535a743dfdc2d
Alex Pletzer <alexander@gokliya.net>
2019-04-02 03:18:37 +0000

    need more work for building mumps with cray compiler

---
+++ maui/easyconfigs/mumps-cray-def.mk
@@ -0,0 +1,15 @@
+export MPICC=cc
+export MPIF77=ftn
+
+export LIBS=""
+
+export BLAS_LIB_DIR=/opt/cray/pe/libsci/18.07.1/CRAY/8.6/x86_64/lib/
+export LIBBLAS=""
+
+export SCALAPACK_LIB_DIR=""
+export LDFLAGS="-L/opt/cray/pe/libsci/18.07.1/CRAY/8.6/x86_64/lib -L/nesi/project/niwa99999/easybuildinstall/XC50_sles12_skl/software/SCOTCH/6.0.4-CrayCCE-18.08/lib -L/nesi/project/niwa99999/easybuildinstall/XC50_sles12_skl/software/METIS/4.0.3-CrayCCE-18.08/lib"
+export FFLAGS="-O2 -fPIC -craype-verbose"
+export CFLAGS="-O2 -fPIC -craype-verbose"
+
+export LIBSCALAPACK=""
+
